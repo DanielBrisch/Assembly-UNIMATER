1b1d75ff8f74da2023e49dbf1aed5e310190332b
DanielBrisch <danielbrisch157@gmail.com>
2024-04-30 21:01:12 -0300

    ativida 1

---
+++ main.asm
@@ -0,0 +1,32 @@
+ORG 000 / inicia programa no endereço 000
+
+Load D
+Store NAME_D
+Load a 
+Store NAME_A
+Load n
+Store NAME_N
+Load i
+Store NAME_I
+Load e
+Store NAME_E
+Load l
+Store NAME_L
+
+Halt / Termina execução 
+
+D,HEX 68
+a,HEX 97
+n,HEX 110
+i,HEX 105
+e,HEX 101
+l,HEX 108
+
+/Reservar o espaço para o Nome Rafa
+
+NAME_D, HEX 0
+NAME_A, HEX 0
+NAME_N, HEX 0
+NAME_I, HEX 0
+NAME_E, HEX 0
+NAME_L, HEX 0
