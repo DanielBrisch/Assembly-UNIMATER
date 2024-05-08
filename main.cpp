e223672fa79376527889023f5389d9a0d939b5ab
DanielBrisch <danielbrisch157@gmail.com>
2024-05-08 00:09:08 -0300

    atividade-3

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+const int relay1 = 12;
+const int relay2 = 7;
+
+void setup()
+{
+    pinMode(relay1, OUTPUT);
+    pinMode(relay2, OUTPUT);
+}
+
+void loop()
+{
+    digitalWrite(relay1, HIGH);
+    delay(1000);
+    digitalWrite(relay1, LOW);
+    delay(1000);
+    digitalWrite(relay2, HIGH);
+    delay(1000);
+    digitalWrite(relay2, LOW);
+    delay(1000);
+}
